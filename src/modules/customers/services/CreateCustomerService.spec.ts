b65374458fa125b4506a4802d1b14b8e7addcc1e
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2021-10-12 17:42:42 -0300

    configurações iniciais para testes automatizados

---
+++ src/modules/customers/services/CreateCustomerService.spec.ts
@@ -0,0 +1,9 @@
+describe('Create customer', () => {
+  it('Should be able to create a new  customer', () => {
+    expect(1).toBe(1);
+  });
+
+  it('Should not be able to create two customers with  the same email', () => {
+    expect(1).toBe(2);
+  });
+});
